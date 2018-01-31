5df97dbd9402d576866398293085531d23dc919b
MakerSquid <solomonchambers@yahoo.com>
2018-01-31 06:58:15 -0700

    changes

---
+++ make-github-safe-regular/FileArchiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MakeGithubSafe
+{
+	class FileArchiver
+	{
+		internal void CreateArchives(List<FileSelection> files)
+		{
+			foreach(var item in files)
+			{
+				ZipAFile(item);
+			}
+		}
+
+		private void ZipAFile(FileSelection file)
+		{
+			//check if zip exists first...
+
+			var process = new Process()
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "7z.exe",
+					Arguments = $"a -v40m \"{file.FilePath}{file.FileName}.zip\" \"{file.FilePath}{file.FileName}\"",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+				}
+			};
+			process.Start();
+			string result = process.StandardOutput.ReadToEnd();
+			process.WaitForExit();
+		}
+	}
+}
---
+++ make-github-safe-regular/FileIterator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MakeGithubSafe
+{
+	class FileIterator
+	{
+		private List<FileSelection> _fileList;
+
+		internal List<FileSelection> GetFilesToPackage()
+		{
+			_fileList = new List<FileSelection>();
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			var basePath = assembly.Location.Remove(assembly.Location.Length - 18);
+
+#if DEVMODE
+			basePath = "C:\\solomon\\notes\\";
+#endif
+
+
+			IteratePath(basePath);
+
+
+
+			return _fileList;
+		}
+
+		private void IteratePath(string path)
+		{
+
+
+            var dirInfo = new DirectoryInfo(basePath);
+
+            var dirList = dirInfo.EnumerateDirectories();
+
+
+            var directoryInfo = provider.GetDirectoryContents(path);
+
+			foreach(var item in directoryInfo)
+			{
+				if (item.IsDirectory)
+				{
+					IteratePath(provider, item.Name);
+				}
+				else
+				{
+					if (item.Length > 41943040)//40 MB
+					{
+						_fileList.Add(new FileSelection { FileName = item.Name, FilePath = GetPathOnly(item.PhysicalPath, item.Name.Length) });
+					}
+				}
+			}
+
+		}
+
+		private string GetPathOnly(string path, int nameLength)
+		{
+			return path.Remove(path.Length - nameLength);
+		}
+	}
+}
