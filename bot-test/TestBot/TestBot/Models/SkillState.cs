33c1dd748f21f396ec44f6ab73afe9ef96760d58
Solomon Chambers <SChambers@ncm.com>
2019-06-10 09:26:31 -0600

    batch file update

---
+++ bot-test/TestBot/TestBot/Models/SkillState.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Luis;
+
+namespace TestBot.Models
+{
+	public class SkillState
+	{
+		public string Token { get; internal set; }
+
+		public TestBotLuis LuisResult { get; internal set; }
+
+		public void Clear()
+		{
+		}
+	}
+}
