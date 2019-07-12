734f27e900060cd243f1e88ff0fcefc92ae53ada
Solomon Chambers <SChambers@ncm.com>
2019-07-12 16:25:36 -0600

    found this

---
+++ bing_csharp/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingRewards
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string AccountName { get; set; }
+        public string Password { get; set; }
+        public int SearchLimit { get; set; }
+        public int AvailablePoints { get; set; }
+        public int LifetimePoints { get; set; }
+    }
+}
